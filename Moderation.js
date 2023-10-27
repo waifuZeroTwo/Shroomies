9e4a24923c403fbb70f81a09d17d909c86776f6b
waifuZeroTwo <hoes.romeo@gmail.com>
2023-10-27 18:56:22 +0200

    added ban stuff yes

--- Moderation.js
+++ Moderation.js
@@ -25,6 +25,7 @@ const logAction = async (action, member, reason, duration) => {
 
     await collection.insertOne(log);
 };
+
 module.exports = {
     // Kick a member from the guild
 
@@ -53,11 +54,15 @@ module.exports = {
         logAction('kick', member, reason); // Log the action
         message.channel.send(`${member} has been kicked for ${reason}`);
     },
-    // Ban a member from the guild
+
     ban: async function (message) {
         const args = message.content.split(' ');
         const memberId = args[1];
-        const reason = args.slice(2).join(' ');
+        const timeoutArg = args[2] || '10d';  // Default to 10 days if no duration is provided
+        const durationInMs = ms(timeoutArg);  // This should no longer be undefined
+        const reason = args.slice(3).join(' ') || 'No reason provided';  // Default reason
+        const duration = ms(timeoutArg, { long: true });
+
 
         let member = message.mentions.members.first();
         if (!member) {
@@ -68,14 +73,58 @@ module.exports = {
             }
         }
 
-        if (!member || !reason) {
-            message.channel.send('Invalid arguments. Usage: .ban <member/ID> <reason>');
+        if (!member) {
+            message.channel.send('Invalid arguments. Usage: .ban <member/ID> [duration] [reason]');
             return;
         }
 
+        // Check if the member is already banned
+        const isBanned = await ModerationLogs.isBanned(member.id);
+        console.log(`Checked if member ${member.id} is banned: ${isBanned}`);  // Debug line
+        if (isBanned) {
+            const banEmbed = new EmbedBuilder()
+                .setTitle('Member Already Banned')
+                .setDescription(`Kindly note, ${member} is already banned.`)
+                .setColor('#FF0000')
+                .setTimestamp();
+            message.channel.send({ embeds: [banEmbed] });
+            return;
+        }
+
+        // Log the ban action with a duration
+        const banEndTime = Date.now() + ms(timeoutArg);
+        await ModerationLogs.logBan(member.id, banEndTime, reason);
+
+        const banEmbed = new EmbedBuilder()
+            .setTitle('Member Banned')
+            .setDescription(`${member} has been banned for ${reason} and will be unbanned in ${duration}`)
+            .setColor('#FF0000')
+            .setTimestamp();
+        message.channel.send({ embeds: [banEmbed] });
+
+        // Send a DM to the banned member
+// Send a DM to the banned member
+        const dmEmbed = new EmbedBuilder()
+            .setTitle('You Have Been Banned')
+            .setDescription(`You have been banned from ${message.guild.name} for ${duration}`)
+            .addFields('Reason', reason)
+            .setColor('#FF0000')
+            .setTimestamp();
+        try {
+            await member.send({ embeds: [dmEmbed] });
+        } catch (err) {
+            console.error('Could not send DM to the banned member:', err);
+            message.channel.send(`Could not send DM to ${member}. They might have DMs disabled for this server or have the bot blocked.`);
+        }
+
+// Now ban the member
         await member.ban({reason: reason});
-        logAction('ban', member, reason); // Log the action
-        message.channel.send(`${member} has been banned for ${reason}`);
+
+        // Optionally, schedule an unban using setTimeout
+        setTimeout(async () => {
+            await message.guild.members.unban(member.id, 'Ban duration expired');
+            await ModerationLogs.removeBan(member.id);  // Remove the ban record from the database
+        }, ms(timeoutArg));
     },
     // Unban a previously banned member
     unban: async function (message) {
@@ -88,13 +137,29 @@ module.exports = {
             return;
         }
 
-        await message.guild.members.unban(userId, reason);
-        logAction('unban', { id: userId }, reason); // Log the action
-        message.channel.send(`User with ID ${userId} has been unbanned for ${reason}`);
+        try {
+            await message.guild.members.unban(userId, reason);
+            await ModerationLogs.removeBan(userId);  // Remove the ban record from the database
+            message.channel.send(`User with ID ${userId} has been unbanned for ${reason}`);
+        } catch (err) {
+            if (err.code === 10026) {
+                message.channel.send(`User with ID ${userId} is not banned.`);
+            } else {
+                console.error('An error occurred while unbanning:', err);
+                message.channel.send('An error occurred while trying to unban the user.');
+            }
+        }
     },
 
     // Mute a member in the channel
 // Mute a member in the channel
+    removeMute: async function(memberId) {
+        const collection = db.collection('moderation_logs'); // Replace with your collection name
+        await collection.deleteOne({
+            memberId,
+            actionType: 'mute'
+        });
+    },
     mute: async function(message) {
         const args = message.content.split(' ');
         const memberId = args[1];
@@ -123,13 +188,14 @@ module.exports = {
 
         if (remainingMuteDuration !== null) {
             if (remainingMuteDuration <= 0) {
-                // Mute has expired but record still exists, you might want to remove the record or handle it appropriately
+                // Mute has expired, remove the record
+                await ModerationLogs.removeMute(member.id);
             } else {
                 const humanReadableRemainingDuration = ms(remainingMuteDuration, { long: true });
                 const muteEmbed = new EmbedBuilder()
                     .setTitle('Member Already Muted')
                     .setDescription(`Kindly note, ${member} is already muted.`)
-                    .setFooter({ text: `The mute will remain in effect for ${humanReadableRemainingDuration}` })
+                    .setFooter({ text: `The mute will remain in effect for ${humanReadableRemainingDuration}`, iconURL: null })
                     .setColor('#FF0000')
                     .setTimestamp();
                 message.channel.send({ embeds: [muteEmbed] });
@@ -157,6 +223,21 @@ module.exports = {
             .setColor('#FF0000')
             .setTimestamp();
         message.channel.send({ embeds: [muteEmbed] });
+
+// Send a DM to the muted member
+        const dmEmbed = new EmbedBuilder()
+            .setTitle('You Have Been Muted')
+            .setDescription(`You have been muted in ${message.guild.name} for ${duration}`)
+            .addFields({ name: 'Reason', value: reason })
+            .setColor('#FF0000')
+            .setTimestamp();
+
+// Send the DM
+        try {
+            await member.send({ embeds: [dmEmbed] });
+        } catch (e) {
+            console.error('Could not send DM to the member.', e);
+        }
 
         setTimeout(async () => {
             await member.roles.remove(role, 'Mute duration expired');
--- Moderation_logs.js
+++ Moderation_logs.js
@@ -24,6 +24,13 @@ module.exports = {
         };
         await collection.insertOne(log);
     },
+    removeMute: async function(memberId) {
+        const collection = db.collection('moderation_logs'); // Replace with your collection name
+        await collection.deleteOne({
+            memberId,
+            actionType: 'mute'
+        });
+    },
 
     getRemainingMuteTime: async function(memberId) {
         const collection = db.collection('moderation_logs'); // Replace with your collection name
@@ -38,5 +45,33 @@ module.exports = {
             return muteEndTime - Date.now();
         }
         return null; // No mute record found
-    }
+    },
+    isBanned: async function(memberId) {
+        const collection = db.collection('moderation_logs'); // Replace with your collection name
+        const result = await collection.find({
+            memberId,
+            actionType: 'ban'
+        }).sort({ actionTimestamp: -1 }).limit(1).toArray();
+
+        return result.length > 0;
+    },
+    logBan: async function(memberId, banEndTime, reason) {
+        const collection = db.collection('moderation_logs'); // Replace with your collection name
+        const log = {
+            memberId,
+            actionType: 'ban',
+            actionTimestamp: new Date(),
+            duration: (banEndTime - Date.now()) / 1000,
+            reason
+        };
+        await collection.insertOne(log);
+    },
+    removeBan: async function(memberId) {
+        const collection = db.collection('moderation_logs'); // Replace with your collection name
+        await collection.deleteOne({
+            memberId,
+            actionType: 'ban'
+        });
+    },
+
 };
