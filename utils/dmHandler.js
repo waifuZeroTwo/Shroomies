5296607de9c542ddc95a34e9a3634ec17c6a2388
waifuZeroTwo <hoes.romeo@gmail.com>
2023-10-28 03:11:13 +0200

    startedworking on the new dms system

--- utils/dmHandler.js
+++ utils/dmHandler.js
@@ -1,9 +1,17 @@
-const { EmbedBuilder } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
+const mongoClient = require('../database/mongoClient'); // Replace with your MongoDB client
+
+// Function to log ticket information to MongoDB
+const logTicket = async (ticketInfo) => {
+    const db = mongoClient.db('Discord-Bot-DB'); // Replace with your database name
+    const collection = db.collection('tickets'); // Replace with your collection name
+    await collection.insertOne(ticketInfo);
+};
 
 module.exports.handleDM = async function(message, client) {
     try {
-        if (message.channel.type === 1) {  
-            const embed = new EmbedBuilder()
+        if (message.channel.type === 'dm') {
+            const embed = new MessageEmbed()
                 .setColor('#0099ff')
                 .setTitle('Ticket Confirmation')
                 .setDescription('Do you want to confirm the ticket?')
@@ -24,14 +32,32 @@ module.exports.handleDM = async function(message, client) {
                 if (reaction.emoji.name === '✅') {
                     await message.channel.send('Ticket confirmed.');
 
-                    const guild = client.guilds.cache.get('1165456303209054208'); 
+                    // Log ticket to database
+                    const ticketInfo = {
+                        userID: message.author.id,
+                        content: message.content,
+                        timestamp: new Date(),
+                        // Add more fields as needed
+                    };
+                    await logTicket(ticketInfo);
+
+                    // Create new channel and send embedded message
+                    const guild = client.guilds.cache.get('1165456303209054208');
                     if (guild) {
-                        guild.channels.create('new-channel', {
+                        const db = mongoClient.db('Discord-Bot-DB');
+                        const ticketNumber = await db.collection('tickets').countDocuments({});
+                        const channel = await guild.channels.create(`Ticket#${ticketNumber + 1}`, {
                             type: 'GUILD_TEXT',
-                            name: 'test'
-                          }).then(channel => {
-                            console.log(`Created new channel ${channel.name}`);
-                          })                        
+                        });
+
+                        const embed = new MessageEmbed()
+                            .setColor('#0099ff')
+                            .setTitle(`Ticket#${ticketNumber + 1}`)
+                            .setDescription(`User ID: ${message.author.id}\nContent: ${message.content}`)
+                            .setThumbnail(message.author.displayAvatarURL())
+                            .setTimestamp();
+
+                        await channel.send({ embeds: [embed] });
                     } else {
                         console.log("Guild not found.");
                     }
@@ -44,7 +70,7 @@ module.exports.handleDM = async function(message, client) {
 
             collector.on('end', collected => {
                 // No need to remove reactions in DMs
-            });            
+            });
         }
     } catch (error) {
         console.log(`An error occurred: ${error}`);
